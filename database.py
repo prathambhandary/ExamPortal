c271264bdb5500070ca35f71ef37b4af20c1315a
Pratham Bhandary <prathamb1501@gmail.com>
2026-05-25 17:40:11 +0530

    Added login function

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 DATABASE = "nextgen.db"
 
@@ -6,16 +7,28 @@ def create_tables():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
 
-    c.execute('''CREATE TABLE IF NOT EXISTS LOGIN (
+    c.execute('''CREATE TABLE IF NOT EXISTS login (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               username TEXT NOT NULL UNIQUE,
               password TEXT NOT NULL,
               role TEXT NOT NULL,
               last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-              )''')
+        )''')
     
     conn.commit()
     conn.close()
+
+def login_user(username, password):
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+
+    c.execute("SELECT password FROM login WHERE username = ?", (username,))
+
+    result = c.fetchone()
+    conn.close()
+    if result:
+        return check_password_hash(result[0], password)
+    return False
 
 if __name__ == "__main__":
     create_tables()
